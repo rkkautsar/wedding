002823d65fe846c5002f0d2e7133458cf7c56aae
Rakha Kanz Kautsar <rakha.kautsar@shopee.com>
2021-06-05 20:50:53 +0800

    remove zoom; doesn't work with safari

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,9 @@
 import * as React from "react";
 import GoogleMapReact from "google-map-react";
-import debounce from "lodash/debounce";
 
 const INITIAL_ZOOM = 12;
 
 export default function Home() {
-  const [zoom, setZoom] = React.useState(INITIAL_ZOOM);
-
-  const debouncedOnChange = React.useCallback(
-    debounce(({ zoom }) => setZoom(zoom), 300),
-    [setZoom]
-  );
-
   return (
     <div className="full-screen-container overflow-hidden">
       <div className="map-bg-container">
@@ -22,10 +14,9 @@ export default function Home() {
             lng: 106.863031,
           }}
           zoom={INITIAL_ZOOM}
-          onChange={debouncedOnChange}
           options={{ mapId: "819c06442ec43f18", disableDefaultUI: true }}
         >
-          <Content lat={-6.626632} lng={106.863031} zoom={zoom} />
+          <Content lat={-6.626632} lng={106.863031} />
         </GoogleMapReact>
       </div>
     </div>
@@ -33,16 +24,8 @@ export default function Home() {
 }
 
 function Content(props: { lat: number; lng: number; zoom: number }) {
-  const { zoom } = props;
-  let scale = Math.floor(100 * (zoom / INITIAL_ZOOM));
   return (
-    <div
-      className="content-container"
-      style={{
-        transform: `scale(${scale}%) translate(-50%, -50%)`,
-        transformOrigin: "center",
-      }}
-    >
+    <div className="content-container">
       <div className="blob z-0 opacity-20" />
       <div className="z-10 text-center grid gap-2">
         <h1 className="text-gray-100 z-10">We're getting married soon!</h1>
